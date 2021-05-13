7579541c3a6f5708e63ae3585b34aaa87b2cfbc0
Mike Ball <mikedball@gmail.com>
2021-05-13 12:10:28 -0400

    add IsOnline method to GraphiteProvider

--- pkg/metrics/providers/graphite.go
+++ pkg/metrics/providers/graphite.go
@@ -17,15 +17,36 @@ limitations under the License.
 package providers
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"net/url"
+	"path"
+	"regexp"
+	"time"
 
 	flaggerv1 "github.com/fluxcd/flagger/pkg/apis/flagger/v1beta1"
 )
 
+const graphiteOnlineQuery = "statsd.*"
+
+type graphiteMetric struct {
+	Leaf          int         `json:"leaf"`
+	Context       interface{} `json:"context"`
+	Text          string      `json:"text"`
+	Expandable    int         `json:"expandable"`
+	ID            string      `json:"id"`
+	AllowChildren int         `json:"allowChildren"`
+}
+
+type graphiteResponse []graphiteMetric
+
 // GraphiteProvider executes Graphite queries.
 type GraphiteProvider struct {
-	url url.URL
+	url     url.URL
+	timeout time.Duration
 }
 
 // NewGraphiteProvider takes a provider spec and returns a Graphite
@@ -37,8 +58,69 @@ func NewGraphiteProvider(provider flaggerv1.MetricTemplateProvider) (*GraphitePr
 	}
 
 	graph := GraphiteProvider{
-		url: *graphiteURL,
+		url:     *graphiteURL,
+		timeout: 5 * time.Second,
 	}
 
 	return &graph, nil
+}
+
+// RunQuery executes the Graphite query and returns the the response.
+func (g *GraphiteProvider) RunQuery(query string) (graphiteResponse, error) {
+	query = url.QueryEscape(g.trimQuery(query))
+	u, err := url.Parse(fmt.Sprintf("./metrics/find?query=%s", query))
+	if err != nil {
+		return nil, fmt.Errorf("url.Parase failed: %w", err)
+	}
+
+	u.Path = path.Join(g.url.Path, u.Path)
+	u = g.url.ResolveReference(u)
+
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("http.NewRequest failed: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(req.Context(), g.timeout)
+	defer cancel()
+
+	r, err := http.DefaultClient.Do(req.WithContext(ctx))
+	if err != nil {
+		return nil, fmt.Errorf("request failed: %w", err)
+	}
+	defer r.Body.Close()
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading body: %w", err)
+	}
+
+	if 400 <= r.StatusCode {
+		return nil, fmt.Errorf("error response: %s", string(b))
+	}
+
+	var result graphiteResponse
+	err = json.Unmarshal(b, &result)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling result: %w, '%s'", err, string(b))
+	}
+
+	return result, nil
+}
+
+// IsOnline runs a simple Graphite query and returns an error if the
+// API is unreachable.
+func (g *GraphiteProvider) IsOnline() (bool, error) {
+	_, err := g.RunQuery(graphiteOnlineQuery)
+	if err != nil {
+		return false, fmt.Errorf("running query failed: %w", err)
+	}
+
+	return true, nil
+}
+
+// trimQuery removes whitespace from the query it's passed.
+func (g *GraphiteProvider) trimQuery(query string) string {
+	space := regexp.MustCompile(`\s+`)
+	return space.ReplaceAllString(query, " ")
 }
