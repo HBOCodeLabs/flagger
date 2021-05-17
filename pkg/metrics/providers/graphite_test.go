6b1ad71475e99e7ca17165b731bdaeae17206bd6
Mike Ball <mikedball@gmail.com>
2021-05-17 11:53:05 -0400

    remove accidentally-left-in `fmt.Println`

--- pkg/metrics/providers/graphite_test.go
+++ pkg/metrics/providers/graphite_test.go
@@ -85,7 +85,6 @@ func TestGraphiteProvider_IsOnline(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				fmt.Println(r.URL.Query().Encode())
 				if r.URL.Path != "/render" || r.URL.Query().Encode() != "format=json&target=test" {
 					w.WriteHeader(http.StatusNotFound)
 					return
